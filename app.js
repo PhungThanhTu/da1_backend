c14b2f8c110b5d4757fa076aea1be5a64fe0032e
phungthanhtu <thanhtucqtuit1952@gmail.com>
2022-07-01 13:14:06 +0700

    cors 2

--- app.js
+++ app.js
@@ -29,7 +29,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
-app.user(cors());
+app.use(cors());
 // api 
 app.use('/auth',authRouter);
 app.use('/user',userRouter);
